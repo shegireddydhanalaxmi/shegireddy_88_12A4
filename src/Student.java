7bf7fd2c50b9a169b581d849276ef1f9e13ae5f8
shegireddydhanalaxmi <31739065+shegireddydhanalaxmi@users.noreply.github.com>
2017-09-08 14:30:58 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -79,3 +79,80 @@ public class Student implements Comparable {
 
 	}
 }
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int nstudents;
+		String stName;
+		int stID;
+		double stmarks;
+		Date stdob;
+
+		System.out.print("How many students would you like to enter?");
+		nstudents = sc.nextInt();
+		sc.nextLine();
+		ArrayList studentArray = new ArrayList(nstudents);
+		
+		for (int i = 0; i < nstudents; i++) {
+			System.out.print("Enter name: ");
+			stName = sc.nextLine();
+			System.out.print("Enter ID: ");
+			stID = sc.nextInt();
+			System.out.print("Enter marks:");
+			stMarks = sc.nextDouble();
+			sc.nextInt();
+			System.out.print("Enter dob:");
+			stDob = sc.nextLine();
+			studentArray.add(new Student( stID, stName,stDob,stMarks));
+		}
+		
+		
+		//System.out.println("Average is: " + Student.getAverage(studentArray));
+		for (ListIterator stIterator = studentArray.listIterator(); stIterator.hasNext(); ) {
+			Student st = (Student)stIterator.next();
+			System.out.println(st);
+		}
+		
+		ListIterator studentIterator = studentArray.listIterator();
+		
+		while (studentIterator.hasNext()) {
+			Student st = (Student)studentIterator.next();
+			System.out.println(st);
+		}
+		
+		System.out.print("Who would you like to delete?");
+		String nameToRemove = sc.nextLine();
+	
+		Student tempStudent = new Student();
+		tempStudent.setName(nameToRemove);
+		int stIndex = studentArray.indexOf(tempStudent);
+		studentArray.remove(stIndex);
+		
+//		for (int i = 0; i < studentArray.size(); i++) {
+//			Student tempStudent = (Student)studentArray.get(i);
+//			if (tempStudent.getName().equals(nameToRemove)) {
+//				studentArray.remove(i);
+//			}
+//		}
+		
+		studentIterator = studentArray.listIterator();
+		
+		while (studentIterator.hasNext()) {
+			Student st = (Student)studentIterator.next();
+			System.out.println(st);
+		}
+		
+		
+		
+//		for (int j = 0; j < studentArray.size(); j++) {
+//			System.out.println(studentArray.get(j));
+//			
+//		}
+		
+		// compute and print out average GPA
+		// print out all students' name, ID and GPA
+		
+	}
+
+}
+
