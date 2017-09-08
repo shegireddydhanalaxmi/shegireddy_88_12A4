2d9958759f15ae7866803328bc8dbaad2a15389d
shegireddydhanalaxmi <31739065+shegireddydhanalaxmi@users.noreply.github.com>
2017-09-08 14:29:28 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -8,53 +8,74 @@ public class Student implements Comparable {
 	private double avgMark;
 
 	public Student(int id, String fullName, Date birthDate, double avgMark) {
+		ID = id;
+		this.fullName = fullName;
+		DOB = birthDate;
+		Marks = avgMark;
 
 	}
 
 	public int getId() {
-
+           return id;
 	}
 
 	public void setId(int id) {
 
+         ID=id;
 	}
 
 	public String getFullName() {
-
+        return fullName;
 	}
 
 	public void setFullName(String fullName) {
-
+        this.fullName = fullName;
 	}
 
 	public Date getBirthDate() {
-
+          return birthDate;
 	}
 
 	public void setBirthDate(Date birthDate) {
-
+          DOB= birthDate;
 	}
 
 	public double getAvgMark() {
-
+        return avgMark;
 	}
 
 	public void setAvgMark(double avgMark) {
-
+         Marks=avgMark;
 	}
 
 	@Override
 	public boolean equals(Object o) {
-
-	}
+		if (this == o) {
+        return true;
+    }
+    if (o == null) {
+        return false;
+    }
+    if (getClass() != obj.getClass()) {
+        return false;
+    }
+    Advertisement o = (Advertisement) o;
+    return this.getId().equals(o.getId());
+}
+}
 
 	@Override
 	public int hashCode() {
-
+         return getId().hashCode();
 	}
 
 	@Override
 	public int compareTo(Object o) {
+		int cmp = getFullName().compareToIgnoreCase(g.getFullName());
+       if (cmp != 0) return cmp;
+        return (getId() < g.getId() ? -1 : getId() == g.getId() ? 0 : 1);
+	}
+}
 
 	}
 }
